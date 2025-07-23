b14869494778d9a5756b77f1101cdecc60731f8b
ilakiya-01 <elakkiyasudha2k@gmail.com>
2025-07-23 13:27:55 +0530

    Cleaned up dependencies for deployment

--- app.py
+++ app.py
@@ -5,20 +5,19 @@ import pickle
 import os
 
 app = Flask(__name__)
-CORS(app)
+CORS(app)  # CORS needed to allow frontend on another origin
 
 model = pickle.load(open("model.pkl", "rb"))
 scaler = pickle.load(open("scaler.pkl", "rb"))
 
-@app.route('/', methods=['GET'])
+@app.route("/", methods=["GET"])
 def home():
-    return "✅ Human Productivity Estimator API is Live!"
+    return "✅ Human Productivity API is live!"
 
-@app.route('/predict', methods=['POST'])
+@app.route("/predict", methods=["POST"])
 def predict():
     try:
         data = request.get_json()
-
         features = np.array([[ 
             data['Sleep_Hours'],
             data['Start_Work_Hour'],
@@ -30,15 +29,12 @@ def predict():
             data['SocialMedia_Min'],
             data['Emails_Handled']
         ]])
-
         scaled = scaler.transform(features)
         prediction = model.predict(scaled)[0]
-
         return jsonify({'productivity_score': round(prediction, 2)})
-
     except Exception as e:
         return jsonify({'error': str(e)})
 
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))  # Railway default
+    app.run(host="0.0.0.0", port=port)

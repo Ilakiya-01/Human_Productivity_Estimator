13fd91995e4372b08ca1dba92152f57589841077
ilakiya-01 <elakkiyasudha2k@gmail.com>
2025-07-23 13:10:56 +0530

    Switch to sep repo for ui for Railway deployment

--- app.py
+++ app.py
@@ -1,17 +1,18 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 import numpy as np
 import pickle
 import os
 
-app = Flask(__name__, template_folder='templates')
+app = Flask(__name__)
 CORS(app)
+
 model = pickle.load(open("model.pkl", "rb"))
 scaler = pickle.load(open("scaler.pkl", "rb"))
 
 @app.route('/', methods=['GET'])
 def home():
-    return render_template('index.html')
+    return "✅ Human Productivity Estimator API is Live!"
 
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -36,7 +37,6 @@ def predict():
         return jsonify({'productivity_score': round(prediction, 2)})
 
     except Exception as e:
-        print("ERROR:", str(e))
         return jsonify({'error': str(e)})
 
 if __name__ == '__main__':
